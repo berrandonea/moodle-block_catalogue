4d00e8f1bf269be611ddea89df03deac613d271b
berrandonea <berrandonea@yahoo.fr>
2016-06-22 10:12:15 +0200

    Add files via upload

--- list/blocks/lang/en/blockcataloguelist_blocks.php
+++ list/blocks/lang/en/blockcataloguelist_blocks.php
@@ -1,1 +1,128 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Initially developped for :
+ * Université de Cergy-Pontoise
+ * 33, boulevard du Port
+ * 95011 Cergy-Pontoise cedex
+ * FRANCE
+ *
+ * Displays a catalogue of all the blocks, modules, reports and customlabels the teacher can use in his course.
+ *
+ * @package    block_catalogue
+ * @author     Brice Errandonea <brice.errandonea@u-cergy.fr>, Salma El-mrabah <salma.el-mrabah@u-cergy.fr>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ *
+ * File : list/blocks/lang/en/blockcataloguelist_blocks.php
+ * English strings for the list of blocks.
+ */
+
+defined('MOODLE_INTERNAL') || die();
+$string['listname'] = 'Blocks';
+$string['monitor'] = 'Monitor learners';
+$string['communicate'] = 'Communicate with learners';
+$string['other'] = 'Other blocks';
+$string['use'] = 'Add new';
+
+$string['description_activity_modules'] = 'An activity is generally an interactive learning segment for a student in a course. The teacher adds activities by a pull down menu found in their course\'s sections when the edit mode is turned on.';
+$string['description_calendar_upcoming'] = 'The Upcoming events block displays future events in a summarized list.';
+$string['description_comments'] = 'The comments block can be added to any page to allow users to add comments.';
+$string['description_community'] = 'The community finder block enables users to access public community hubs, such as MOOCH, and search for courses to download or enrol in.';
+$string['description_blog_menu'] = 'The blog menu block provides links to View all my entries Add a new entry plus context-sensitive links for adding an entry about the course or a particular activity.';
+$string['description_blog_tags'] = "A Blog Tags block displays a list of blogs where font size visually indicates each blog's use.";
+$string['description_course_summary'] = "The course/site summary block provides a summary and/or description for your course as contained in the summary text of the course settings.";
+$string['description_feedback'] = "The Feedback block may be added to courses to provide a quick link to global feedback activities which have been set up from the front page.";
+$string['description_glossary_random'] = "The random glossary block can be used to display random entries from a glossary, which usually take the form of dictionary style definitions.";
+$string['description_login'] = "The Login block provides logged out users an in-site area to enter their Username and Password and login, Create a new account or retrieve/reset their password.";
+$string['description_participants'] = "Participants are the teachers and students who are part of a course.";
+$string['description_myexisting'] = "SEFIAP";
+$string['description_catalogue'] = "SEFIAP";
+$string['description_myprofile'] = "The logged in user block displays certain information about the user who is currently logged in to a Moodle course.";
+$string['description_course_main_menu'] = "The Course Menu block for Moodle provides navigation between course sections (topics/weeks)";
+$string['description_mnet_hosts'] = "The Network Servers block allows you to roam to other Moodle (or Mahara) servers.";
+$string['description_settings'] = "The settings block provides context-sensitive links to settings pages.";
+$string['description_eval_main_menu'] = 'SEFIAP';
+$string['description_enrol_demands'] = 'SEFIAP';
+$string['description_course_contents'] = "Course contents block produces a table of contents for the course - that is a list of all visible sections (topics or weeks) in the course.";
+$string['description_course_list'] = "The Courses block lists and allows navigation between all of the courses in which the logged in user is a participant (as tutor and/or student).";
+$string['description_course_overview'] = "The Course overview block is visible on a user's My home page and shows a list of all courses in which a user is enrolled/has an assigned role.";
+$string['description_html'] = "A HTML block is a standard block used to add text or images on a site or course page. The title bar can be left blank.";
+$string['description_private_files'] = "The My private files block enables access to a user's private files area.";
+$string['description_blog_recent'] = "This block can be configured to display the last N blog entries, filtered by context.";
+$string['description_quiz_results'] = "The quiz results block displays the highest and/or lowest grades achieved on a quiz within a course. There must be a quiz in the course to correctly configure this block.";
+$string['description_recent_activity'] = "The Recent activity block lists course activity, such as updated resources and activities, forum posts and assignment submissions, since the user last accessed the course.";
+$string['description_section_links'] = "The Section links block helps the student or teacher to quickly navigate to a particular topic/week section of the course.";
+$string['description_site_main_menu'] = "The main menu block can be added to the front page of the site by a site administrator.";
+$string['description_social_activities'] = "This block enables additional activities to be added to a course in social format.";
+$string['description_activity_results'] = "The Activity results block displays results from graded or rated activities in a course.";
+$string['description_calendar_month'] = "Displays events for the current month.";
+$string['description_online_users'] = "The Online users block shows a list of users who have been logged into the current course.";
+$string['description_mentees'] = "A Mentees block is a frontpage block that provides mentors with quick access to their mentee(s) profile page(s).";
+$string['description_completionstatus'] = "The course completion status block shows what has been done towards completing the course. The student and teacher will both see this block.";
+$string['description_messages'] = "The message block displays a list of new messages you have received, with a link to your Messages window.";
+$string['description_rss_client'] = "The RSS feeds block enables RSS feeds from external websites to be displayed within Moodle.";
+$string['description_search_forums'] = "The Search forum block allows you to search the course forums for a word or phrase. Type the word or phrase you want to find in the text field space.";
+$string['description_selfcompletion'] = "The self completion block provides a link for students to declare that they have completed the course. This may be part of the Course completion requirements.";
+$string['description_badges'] = "The latest badges block can be added to the Site front page, a user's Dashboard or a course page and will display badges earned.";
+$string['description_news_items'] = "Recent posts made in the News forum are displayed in the Latest news block, along with a link to older archived news.";
+$string['description_navigation'] = "The navigation block appears on every page of the site. It contains an expanding tree menu which includes My Home, Site Pages, My Profile, and Courses.";
+$string['description_tag_youtube'] = "The Youtube block can only be added to a Tags page. Once you have edited your Profile or made a Blog entry and provided key words in the interests' portion, these words become tags.";
+$string['description_tag_flickr'] = "The Flickr block can only be added to a Tags page. The Flickr block settings allow you to change the images that are displayed in the block based on Relevance (default), Date Posted, Date Taken and Interestingness.";
+$string['description_vet_events'] = "SEFIAP";
+$string['description_tags'] = "A very effective way of viewing all tags is in a tags block, or tag cloud. A unique feature of tag clouds are that the size of each tag is related to the number of items associated with it.";
+$string['description_conditional_course'] = "SEFIAP";
+$string['description_admin_bookmarks'] = "The intent of this block is to bookmark pages so that an administrator role can see them as a list of links.";
+
+$string['link_activity_modules'] = 'en/Activities_block';
+$string['link_activity_results'] = 'en/Activity_results_block';
+$string['link_admin_bookmarks'] = 'en/Admin_bookmarks_block';
+$string['link_badges'] = 'en/My_latest_badges_block';
+$string['link_blog_menu'] = 'en/Blog_menu_block';
+$string['link_blog_recent'] = 'en/Recent_blog_entries_block';
+$string['link_blog_tags'] = 'en/Blog_tags_block';
+$string['link_calendar_month'] = 'en/Calendar_block';
+$string['link_calendar_upcoming'] = 'en/Upcoming_events_block';
+$string['link_comments'] = 'en/Comments_block';
+$string['link_community'] = 'en/Community_finder_block';
+$string['link_completionstatus'] = 'en/Course_completion_status_block';
+$string['link_course_list'] = 'en/Courses_block';
+$string['link_course_overview'] = 'en/Course_overview_block';
+$string['link_feedback'] = 'en/Feedback_block';
+$string['link_glossary_random'] = 'en/Random_glossary_entry_block';
+$string['link_html'] = 'en/HTML_block';
+$string['link_login'] = 'en/Login_block';
+$string['link_mentees'] = 'en/Mentees_block';
+$string['link_messages'] = 'en/Messages_block';
+$string['link_mnet_hosts'] = 'en/Network_servers_block';
+$string['link_myprofile'] = 'en/Logged_in_user_block';
+$string['link_navigation'] = 'en/Navigation_block';
+$string['link_news_items'] = 'en/Latest_news_block';
+$string['link_online_users'] = 'en/Online_users_block';
+$string['link_participants'] = 'en/People_block';
+$string['link_private_files'] = 'en/Private_files_block';
+$string['link_quiz_results'] = 'en/Quiz_results_block';
+$string['link_recent_activity'] = 'en/Recent_activity_block';
+$string['link_rss_client'] = 'en/Remote_RSS_feeds_block';
+$string['link_search_forums'] = 'en/Search_forums_block';
+$string['link_section_links'] = 'en/Section_links_block';
+$string['link_selfcompletion'] = 'en/Self_completion_block';
+$string['link_site_main_menu'] = 'en/Main_menu_block';
+$string['link_social_activities'] = 'en/Social_activities_block';
+$string['link_tags'] = 'en/Tags_block';
+$string['link_tag_flickr'] = 'en/Flickr_block';
+$string['link_tag_youtube'] = 'en/Youtube_block';
 
