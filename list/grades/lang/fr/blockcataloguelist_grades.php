72c82a7802dcad4721daa9ebdf982951a12e1cfb
berrandonea <berrandonea@yahoo.fr>
2016-06-22 10:44:49 +0200

    Add files via upload

--- list/grades/lang/fr/blockcataloguelist_grades.php
+++ list/grades/lang/fr/blockcataloguelist_grades.php
@@ -1,1 +1,53 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
+/**
+ * Initially developped for :
+ * Université de Cergy-Pontoise
+ * 33, boulevard du Port
+ * 95011 Cergy-Pontoise cedex
+ * FRANCE
+ *
+ * Displays a catalogue of all the blocks, modules, reports and customlabels the teacher can use in his course.
+ *
+ * @package    block_catalogue
+ * @author     Brice Errandonea <brice.errandonea@u-cergy.fr>, Salma El-mrabah <salma.el-mrabah@u-cergy.fr>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ *
+ * File : list/reports/lang/fr/blockcataloguelist_blocks.php
+ * French strings for the list of reports.
+ */
+
+defined('MOODLE_INTERNAL') || die();
+$string['listname'] = 'Notes';
+$string['gradereport'] = 'Rapports sur les notes';
+$string['gradesetting'] = 'Réglages des notes';
+$string['outcome'] = 'Objectifs, compétences, badges';
+$string['use'] = 'Consulter';
+
+$string['description_gradesetting_scale'] = "Un barème est la liste des notes ou appréciations qu'un apprenant peut recevoir pour une activité.";
+$string['description_gradesetting_tree'] = "Permet de modifier les coefficients de chaque activité notée, l'affichage des notes, la pondération et le mode de calcul des notes.";
+$string['description_gradesetting_letter'] = "Système de notation anglo-saxon (A, B, C, D, E, F)";
+$string['description_gradesetting_settings'] = "Permet de paramétrer le carnet de note des apprenants.";
+$string['description_gradereport_singleview'] = "Affiche les notes pour un étudiant dans une activité.";
+$string['description_gradereport_history'] = "Récapitule les modifications apportées aux notes.";
+$string['description_gradereport_overview'] = "Vue globale des notes de l'apprenant sur l'ensemble des cours sur la plateforme.";
+$string['description_gradereport_grader'] = "Les notes des apprenants sur le cours.";
+$string['description_gradereport_user'] = "Permet de voir les notes d'un participant sur l'ensemble des activités notées du cours.";
+$string['description_badges_index'] = "Créer et gérer des badges pour ce cours, avec leurs critères d'attribution aux étudiants.";
+$string['description_gradesetting_outcome'] = "Montre les objectifs définis sur la plateforme et disponibles pour votre cours.";
+$string['description_gradesetting_outcomecourse'] = "Montre les objectifs utilisés dans votre cours.";
+$string['description_gradereport_outcomes'] = "Montre dans quelle proportion chaque objectif a été atteint par les étudiants.";
