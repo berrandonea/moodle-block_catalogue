405a5de3d773d6e63755d245a713492813ec4939
berrandonea <berrandonea@yahoo.fr>
2016-06-30 10:57:36 +0200

    Add files via upload

--- list/reports/lang/en/blockcataloguelist_reports.php
+++ list/reports/lang/en/blockcataloguelist_reports.php
@@ -35,3 +35,15 @@ defined('MOODLE_INTERNAL') || die();
 $string['listname'] = 'Reports';
 $string['report'] = 'Reports';
 $string['use'] = 'Check';
+
+$string['description_report_progress'] = "Did this learner complete/view this activity/resource ?";
+$string['description_report_completion'] = "Shows a student's progress through the course, against specific criteria.";
+$string['description_report_outline'] = "Shows all the activities and resources in the course, sorted out by section.";
+$string['description_report_log'] = "Lists the actions performed in this course by the learners and teachers.";
+$string['description_report_loglive'] = "To follow the actions performed right now in the course.";
+$string['description_report_engagement'] = "Follow the students engagement against a set of indicators.";
+$string['description_report_participation'] = "Shows the learners participation in the cours, sorted by activity module, status and group.";
+$string['description_report_stats'] = "Some figures about the course.";
+$string['description_report_exportlist'] = "Export the students list for a group (or the whole course) as a CSV file.";
+$string['description_report_courseoverview'] = "Shows in which courses a given user is enroled, with notes if there are things to do in theses courses.";
+$string['description_report_roster'] = 'Roster of the students. Names can be hidden to help you learn them.';
