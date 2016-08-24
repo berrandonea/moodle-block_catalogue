19b12333659ab87ab56abd8e9b136d15ee21bd23
Brice Errandonea <berrandonea@yahoo.fr>
2016-08-24 14:05:55 +0200

    Update version number

--- version.php
+++ version.php
@@ -23,7 +23,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component  = 'block_catalogue';
-$plugin->release    = '1.0.1';
-$plugin->version    = 2016081000;
-$plugin->requires   = 2014051200;
+$plugin->release    = '1.1';
+$plugin->version    = 2016082400;
+$plugin->requires   = 2014022000);
 $plugin->maturity   = MATURITY_STABLE;
